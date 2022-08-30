6554559a949e73b522cf188f13000e949fa86ab9
Ivan Dmitriev <wolf1848@mail.ru>
2022-08-30 22:19:38 +1000

    add front

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -3,15 +3,14 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"net/http"
-	"startGo/cmd/web/routes"
 )
 
 func main() {
 	e := echo.New()
 
-	e.GET("/users/:id", routes.GetUser)
+	e.Static("/", "web/front/dist")
 
-	if err := e.Start(":8080"); err != http.ErrServerClosed {
+	if err := e.Start(":3125"); err != http.ErrServerClosed {
 		e.Logger.Fatal(err)
 	}
 }
--- internal/routes.go
+++ internal/routes.go
@@ -1,4 +1,4 @@
-package routes
+package internal
 
 import (
 	"github.com/labstack/echo/v4"
