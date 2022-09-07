04c5450255a7df6c3acbe757d3b94e7ad406a6f4
Ivan Dmitriev <wolf1848@mail.ru>
2022-09-07 22:44:36 +1000

    database

--- internal/repository/user_postgres.go
+++ internal/repository/user_postgres.go
@@ -1,1 +1,7 @@
 package repository
+
+type Users struct {
+	id       int32  `json:"id"`
+	login    string `json:"login"`
+	fullName string `json:"full_name,omitempty"`
+}
--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,8 +1,21 @@
 CREATE TABLE users(
     id serial primary key,
-    name varchar(255) not null,
-    login varchar(255) not null,
-    age integer not null
+    login varchar(30) not null unique,
+    full_name varchar(255) null,
+    hash_pwd varchar(255) not null
 );
 
-insert into users (name,login,age) values ('ivan','livan',17),('katarsis','ekt',152);
+CREATE TABLE info (
+    id serial primary key,
+    hash_login varchar(255) not null,
+    hash_pwd varchar(255) not null,
+);
+
+
+//login
+//password
+//project Проект где используется
+//source Источник доступа
+//type_source Тип ресурса
+//type_access тип доступа (основной, вторичный)
+// comment комментарий
